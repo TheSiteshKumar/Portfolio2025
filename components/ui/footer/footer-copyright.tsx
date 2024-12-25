52f45e10d84c6a69f9f618b72fb2854e84b98db9
Sitesh <thesiteshkumar@gmail.com>
2024-12-25 22:54:21 +0530

    copyright

--- components/ui/footer/footer-copyright.tsx
+++ components/ui/footer/footer-copyright.tsx
@@ -7,12 +7,12 @@ interface FooterCopyrightProps {
   authorName: string;
 }
 
-export function FooterCopyright({ companyName, authorName }: FooterCopyrightProps) {
+export function FooterCopyright({ authorName }: FooterCopyrightProps) {
   const currentYear = new Date().getFullYear();
   
   return (
     <p className="text-sm text-gray-400">
-      © {currentYear} {companyName}. Created with{" "}
+      © {currentYear}, Created with{" "}
       <Heart className="inline-block w-4 h-4 text-red-500 mx-1" />{" "}
       by {authorName}
     </p>
